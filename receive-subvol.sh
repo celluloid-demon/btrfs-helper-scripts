911ad2a96a065529d82a5c28591262e6a454e9f7
jonathan <celluloid-demon@users.noreply.github.com>
2024-01-02 02:26:40 -0800

    tweaks

--- receive-subvol.sh
+++ receive-subvol.sh
@@ -4,11 +4,13 @@
 set -e
 
 snapshot_dir="/mnt/@ds0/snapshots/lenovo-legion/@home/snapshots"
-snapshot_num="1"
+# snapshot_num="1"
+snapshot_num="2"
 # subvol_file="out.dat"
 subvol_file="out.diff"
 working_dir="/mnt/@ds0/@buffer"
 
 cd "$working_dir"
 
+sudo mkdir "${snapshot_dir}/${snapshot_num}"
 sudo cat "./${subvol_file}" | sudo btrfs receive "${snapshot_dir}/${snapshot_num}"

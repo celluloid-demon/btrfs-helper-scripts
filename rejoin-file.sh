72190be7e7939e828b4455df8bc9e851676ff385
jonathan <celluloid-demon@users.noreply.github.com>
2024-01-01 23:35:13 -0800

    added push

--- rejoin-file.sh
+++ rejoin-file.sh
@@ -3,14 +3,9 @@
 # Exit on error
 set -e
 
-input_file="out.dat.gz"
-part_size="256M"
-working_dir="/home/@buffer"
-
-prefix="${input_file}."
+input_basename="out.dat.gz"
+working_dir="/mnt/@ds0/@buffer"
 
 cd "$working_dir"
 
-sudo split --bytes="$part_size" --numeric-suffixes --suffix-length=4 --additional-suffix=.part "./${input_file}" "$prefix"
-
-sudo rm "./${input_file}"
+sudo cat ${input_basename}* > ./${input_basename}

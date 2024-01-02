48985bdc0fee9c237623ec227955f9bc371f05d5
jonathan <celluloid-demon@users.noreply.github.com>
2024-01-02 01:56:32 -0800

    tweaks

--- receive-subvol.sh
+++ receive-subvol.sh
@@ -4,8 +4,10 @@
 set -e
 
 snapshot_dir="/mnt/@ds0/snapshots/lenovo-legion/@home/snapshots"
-snapshot_num="1"
-subvol_file="out.dat"
+# snapshot_num="1"
+snapshot_num="2"
+# subvol_file="out.dat"
+subvol_file="out.diff"
 working_dir="/mnt/@ds0/@buffer"
 
 cd "$working_dir"

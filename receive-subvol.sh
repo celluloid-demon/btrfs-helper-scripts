6ac1c1f165b59884cbf897af32784ce656426742
jonathan <celluloid-demon@users.noreply.github.com>
2024-01-02 00:31:18 -0800

    receive-subvol improvements

--- receive-subvol.sh
+++ receive-subvol.sh
@@ -3,10 +3,11 @@
 # Exit on error
 set -e
 
-snapshot_dir="/mnt/@ds0/backups/lenovo-legion/jonathan/@home/snapshots/1/snapshot"
+snapshot_dir="/mnt/@ds0/snapshots/lenovo-legion/@home/snapshots"
+snapshot_num="1"
 subvol_file="out.dat"
 working_dir="/mnt/@ds0/@buffer"
 
 cd "$working_dir"
 
-sudo cat "./${subvol_file}" | sudo btrfs receive "$snapshot_dir"
+sudo cat "./${subvol_file}" | sudo btrfs receive "${snapshot_dir}/${snapshot_num}"
